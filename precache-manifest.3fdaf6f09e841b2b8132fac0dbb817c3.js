afb4a19095b0e11d5cb5491a37641ef5fe94c471
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-05-06 09:10:26 +0200

    Updates

---
+++ precache-manifest.3fdaf6f09e841b2b8132fac0dbb817c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a56079ac8c03f6b16d9e09c40fb536",
+    "url": "/React-7/index.html"
+  },
+  {
+    "revision": "97be2970d9931f7c3d05",
+    "url": "/React-7/static/css/main.775b3130.chunk.css"
+  },
+  {
+    "revision": "cf19cbea2e401216f7ba",
+    "url": "/React-7/static/js/2.fe33045a.chunk.js"
+  },
+  {
+    "revision": "97be2970d9931f7c3d05",
+    "url": "/React-7/static/js/main.aad13a93.chunk.js"
+  },
+  {
+    "revision": "62fcd21fb1c5bf70abef",
+    "url": "/React-7/static/js/runtime~main.efaf2ddd.js"
+  }
+]);
